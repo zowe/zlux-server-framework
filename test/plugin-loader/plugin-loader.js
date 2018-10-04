21f202a925bcdb3fca327abc6bef2f14ecc3c703
Fyodor Kovin <fkovin@rocketsoftware.com>
2018-10-04 20:26:44 +0500

    Unit tests for the plugin loader
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

---
+++ test/plugin-loader/plugin-loader.js
@@ -0,0 +1,124 @@
+const assert = require('assert')
+const makePlugin = require('plugin-loader').makePlugin
+  
+describe('Plugin', function() {
+  const pluginContext = {
+    productCode: "XYZ",
+    config: {},
+    authManager: {}
+  };
+  
+  describe('makePlugin', function() {
+    
+    //TODO more tests
+    
+    it('should correctly process a basic plugin def', function() {
+      const testPluginDef = {
+        "identifier": "com.rs.testplugin",
+        "baseURI": "testplugin",
+        "apiVersion": "1.0.0",
+        "pluginVersion": "1.0.0",
+        "pluginType": "application",
+      };
+      const p = makePlugin(testPluginDef, {}, pluginContext, false);
+      assert.equal(p.identifier, "com.rs.testplugin");
+      assert.equal(p.pluginVersion, "1.0.0");
+      assert.equal(p.pluginType, "application");
+      assert.equal(p.constructor.name, "ApplicationPlugIn");
+    }),
+    
+    it('should complain about a plugin def without an ID', function() {
+      /*
+       * TODO other invalid combinations
+       */
+      const testPluginDef = {
+        "baseURI": "testplugin",
+        "apiVersion": "1.0.0",
+        "pluginVersion": "1.0.0",
+        "pluginType": "application",
+      };
+      assert.throws(() => {
+        makePlugin(testPluginDef, {}, pluginContext, false);
+      });
+    }),
+    
+    it('should correctly group services', function() {
+      const testPluginDef = {
+        "identifier": "com.rs.testplugin",
+        "baseURI": "testplugin",
+        "apiVersion": "1.0.0",
+        "pluginVersion": "1.0.0",
+        "pluginType": "application",
+        "dataServices": [
+          {
+            "type": "router",
+            "name": "foo",
+            "fileName": "nop-router.js",
+            "version": "1.2.3"
+          },
+          {
+            "type": "router",
+            "name": "foo",
+            "fileName": "nop-router.js",
+            "version": "4.5.6"
+          },
+          {
+            "type": "router",
+            "name": "bar",
+            "fileName": "nop-router.js",
+            "version": "4.4.4"
+          }
+        ]
+      };
+      const p = makePlugin(testPluginDef, {}, pluginContext, false);
+      assert(p.dataServicesGrouped)
+      assert(p.dataServicesGrouped['foo'])
+      assert.equal(p.dataServicesGrouped['foo'].highestVersion, '4.5.6');
+      assert(p.dataServicesGrouped['foo'].versions['1.2.3'])
+      assert.equal(p.dataServicesGrouped['foo'].versions['1.2.3'].name, 'foo')
+      assert(p.dataServicesGrouped['foo'].versions['4.5.6'])
+      assert.equal(p.dataServicesGrouped['foo'].versions['4.5.6'].name, 'foo')
+      assert(p.dataServicesGrouped['bar'])
+      assert.equal(p.dataServicesGrouped['bar'].highestVersion, '4.4.4');
+      assert(p.dataServicesGrouped['bar'].versions['4.4.4'])
+      assert.equal(p.dataServicesGrouped['bar'].versions['4.4.4'].name, 'bar')
+      assert(!p.dataServicesGrouped['blah'])
+      assert(!p.dataServicesGrouped['something'])
+    }),
+    
+    it('should correctly group imports', function() {
+      const testPluginDef = {
+        "identifier": "com.rs.testplugin",
+        "baseURI": "testplugin",
+        "apiVersion": "1.0.0",
+        "pluginVersion": "1.0.0",
+        "pluginType": "application",
+        "dataServices": [
+          {
+            "type": "import",
+            "sourcePlugin": "com.rs.provider",
+            "sourceName": "blah",
+            "versionRange": "2.0.0",
+            "version": "2.0.0",
+            "localName": "blah"
+          },
+          {
+            "type": "import",
+            "sourcePlugin": "com.rs.intermediary",
+            "sourceName": "something",
+            "versionRange": "1.0.0",
+            "version": "1.0.0",
+            "localName": "something"
+          }
+        ]
+      };
+      const p = makePlugin(testPluginDef, {}, pluginContext, false);
+      assert(p.importsGrouped['blah'])
+      assert.equal(p.importsGrouped['blah'].highestVersion, '2.0.0');
+      assert(p.importsGrouped['blah'].versions['2.0.0'])
+      assert(p.importsGrouped['something'])
+      assert.equal(p.importsGrouped['something'].highestVersion, '1.0.0');
+      assert(p.importsGrouped['something'].versions['1.0.0'])
+    })    
+  })
+})
