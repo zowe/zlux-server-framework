2174a2d21e9c81271f407b89a05d8b13e1f35ab7
Fyodor Kovin <fkovin@rocketsoftware.com>
2018-12-13 15:12:52 +0100

    Add some comments and fix a bug
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

--- js/webauth.js
+++ js/webauth.js
@@ -12,7 +12,6 @@ const Promise = require('bluebird');
 const util = require('./util');
 const UNP = require('./unp-constants');
 
-
 const authLogger = util.loggers.authLogger;
 
 function getAuthHandler(req, authManager) {
@@ -40,6 +39,8 @@ function getAuthPluginSession(req, pluginID, dflt) {
 
 function setAuthPluginSession(req, pluginID, authPluginSession) {
   if (req.session) {
+    // Note that it does something only when req.session[pluginID] is 
+    // undefined. Otherwise it does nothing (see getAuthPluginSession()) 
     req.session[pluginID] = authPluginSession;
   }
 }
@@ -168,11 +169,11 @@ module.exports = function(authManager) {
           const authPluginSession = getAuthPluginSession(req, pluginID, {});
           req[`${UNP.APP_NAME}Data`].plugin.services = 
             authServiceHandleMaps[pluginID];
-          const wasAuthenticate = authPluginSession.authenticated;
+          const wasAuthenticated = authPluginSession.authenticated;
           const handlerResult = yield handler.authenticate(req, 
               authPluginSession);
           //do not modify session if not authenticated or deauthenticated
-          if (wasAuthenticate || authPluginSession.authenticated) {
+          if (wasAuthenticated || authPluginSession.success) {
             setAuthPluginSession(req, pluginID, authPluginSession);
           }
           result.addHandlerResult(handlerResult, handler);
