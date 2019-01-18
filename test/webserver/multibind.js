a1ad26ab5261e328a84cb1000de3171d6fec2702
Jordan Filteau <jfilteau@rocketsoftware.com>
2019-01-18 16:28:19 -0500

    copyright
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- test/webserver/multibind.js
+++ test/webserver/multibind.js
@@ -1,3 +1,13 @@
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
 const assert = require('assert')
 const zluxUtil = require('../../lib/util.js')
 const pd = "..\..\..\\zlux-app-server\\deploy\\product"
@@ -90,3 +100,13 @@ describe('getLoopbackAddress', function() {
   });
     
 });
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
