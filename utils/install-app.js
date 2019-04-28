1ba6e28a113bf7f8f96fe6873c6a54c7681d317b
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-04-27 20:30:15 -0400

    Yanked packaging code until decisions made about packaging formats
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- utils/install-app.js
+++ utils/install-app.js
@@ -8,7 +8,6 @@
  Copyright Contributors to the Zowe Project.
 */
 
-const yauzl = require('yauzl');
 const fs = require('graceful-fs');
 const Promise = require('bluebird');
 const path = require('path');
@@ -18,11 +17,11 @@ const jsonUtils = require('../lib/jsonUtils');
 const rmrf = require('rimraf');
 
 //assuming that this is file isnt being called from another that is already using the logger... else expect strange logs
-const logger = packagingUtils.coreLogger.makeComponentLogger("unpackage-app"); //should only need one for this program
+const logger = packagingUtils.coreLogger.makeComponentLogger("install-app"); //should only need one for this program
 
 const argParser = require('../lib/argumentParser');
-const usage = 'Usage: --inputApp | -i INPUTAPP --outputDir | -o OUTPUTDIR --pluginsDir | -p PLUGINSDIR '
-      + '--zluxConfig | -c ZLUXCONFIGPATH [--verbose | -v] [--overwrite | -w]';
+const usage = 'Usage: --inputApp | -i INPUTAPP --pluginsDir | -p PLUGINSDIR '
+      + '--zluxConfig | -c ZLUXCONFIGPATH [--verbose | -v]';
 
 //TODO if plugins get extracted read-only, then how would we go about doing upgrades? read-write for now!
 const FILE_WRITE_MODE = 0o600;
@@ -30,11 +29,9 @@ const DIR_WRITE_MODE = 0o700;
 
 const OPTION_ARGS = [
   new argParser.CLIArgument('inputApp', 'i', argParser.constants.ARG_TYPE_VALUE),
-  new argParser.CLIArgument('outputDir', 'o', argParser.constants.ARG_TYPE_VALUE),
   new argParser.CLIArgument('pluginsDir', 'p', argParser.constants.ARG_TYPE_VALUE),
   new argParser.CLIArgument('zluxConfig', 'c', argParser.constants.ARG_TYPE_VALUE),
-  new argParser.CLIArgument('verbose', 'v', argParser.constants.ARG_TYPE_FLAG),
-  new argParser.CLIArgument('overwrite', 'w', argParser.constants.ARG_TYPE_FLAG)
+  new argParser.CLIArgument('verbose', 'v', argParser.constants.ARG_TYPE_FLAG)
 ];
 
 const commandArgs = process.argv.slice(2);
@@ -48,7 +45,7 @@ if (!userInput.inputApp || !userInput.outputDir ||
 }
 
 if (userInput.verbose) {
-  packagingUtils.coreLogger.setLogLevelForComponentName('unpackage-app', logger.FINE);
+  packagingUtils.coreLogger.setLogLevelForComponentName('install-app', logger.FINE);
 }
 
 userInput.inputApp = serverUtils.normalizePath(userInput.inputApp);
@@ -79,22 +76,6 @@ function isFile(path) {
   return false;
 }
 
-function checkDestinationFolder(dir, overwrite) {
-  try {
-    fs.statSync(dir);
-    if (!overwrite) {
-      packagingUtils.endWithMessage(
-        `Destination or temp folder ${dir} exists. To overwrite for upgrade, run with flag --overwrite`);
-    }
-  } catch (e) {
-    if (e.code != 'ENOENT') {
-      packagingUtils.endWithMessage(`Could not stat destination or temp folder ${dir}. Error=${e.message}`);
-    }
-    //otherwise, not existing is what we want
-  }
-}
-
-
 function cleanup() {
   logger.warn(`Cleanup not yet implemented`);
 }
@@ -121,62 +102,6 @@ function addToServer(appDir) {
   }
 }
 
-function extract(appPath, appDir) {
-  yauzl.open(appPath, {lazyEntries: true}, function(err, zipfile) {
-    if (err) {
-      packagingUtils.endWithMessage(`Could not open App ${userInput.inputApp}. Error=${err.message}`);
-    }
-    zipfile.on("close", function() {
-      try {
-        rmrf(appDir, (err)=> {
-          if (err) {
-            cleanup();
-            packagingUtils.endWithMessage(
-              `Could not remove old App for upgrade, in folder ${appDir}. Error=${err.message}`);
-          }
-          fs.renameSync(appDir+'.temp',appDir);
-          logger.info(`Extracted App to ${appDir}`);
-          addToServer(appDir);
-        });
-      } catch (e) {
-        packagingUtils.endWithMessage(
-          `Could not rename temp folder ${appDir+'.temp'} to ${appDir}. Error=${e.message}`);
-      }
-    });  
-    zipfile.readEntry();
-    zipfile.on("entry", function(entry) {
-      if (entry.fileName.endsWith('/')) {
-        //directory
-        packagingUtils.mkdirp(path.join(appDir+'.temp',entry.fileName), {mode: DIR_WRITE_MODE}).then(()=> {
-          zipfile.readEntry();
-        }).catch((err)=> {
-          cleanup();
-          packagingUtils.endWithMessage(
-            `App could not be installed, extraction failed during directory creation. Error=${err}`);
-        });
-      } else {
-        //file
-        packagingUtils.mkdirp(path.join(appDir+'.temp',path.dirname(entry.fileName)), {mode: DIR_WRITE_MODE}).then(()=>{
-          zipfile.openReadStream(entry, function(err, readStream) {
-            if (err) {
-              cleanup();
-              packagingUtils.endWithMessage(
-                `App could not be installed, extraction failed during archived file read. Error=${err}`);
-            }
-            logger.debug(`Writing: ${entry.fileName}, Size=${entry.uncompressedSize}`);
-            let writeStream = fs.createWriteStream(path.join(appDir+'.temp',entry.fileName), {mode: FILE_WRITE_MODE});
-            writeStream.on("close", ()=> {
-              logger.debug(`Wrote: ${entry.fileName}`);
-              zipfile.readEntry();
-            });
-            readStream.pipe(writeStream);
-          });        
-        });
-      }
-    });
-  });
-}
-
 if (isFile(pluginsDir)) {
   packagingUtils.endWithMessage(`App Server plugins directory location given (${pluginsDir}) is not a directory.`);
 }
@@ -185,17 +110,7 @@ if (!isFile(userInput.inputApp)) {
   const pluginDefinition = packagingUtils.validatePluginInDirectory(userInput.inputApp);
   addToServer(userInput.inputApp);  
 } else {
-  let appDirName = path.basename(userInput.inputApp);
-  let extensionPos = appDirName.lastIndexOf('.');
-  if (extensionPos != -1) {
-    appDirName = appDirName.substring(0,extensionPos);
-  }
-  const appDir = path.join(userInput.outputDir,appDirName);
-
-  checkDestinationFolder(appDir, userInput.overwrite);
-  checkDestinationFolder(appDir+'.temp', true); //which will be renamed upon completion
-
-  extract(userInput.inputApp, appDir);
+  packagingUtils.endWithMessage(`App given was not a directory. Not yet implemented: Package extraction`);
 }
 
 /*
