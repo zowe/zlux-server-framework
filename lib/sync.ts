ae81f6cab6fc6dc9e85a12c0d8d34af6a90a45ee
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-04-08 17:17:52 +0500

    Sync Node Servers: First attempt to pick up session changes on backup
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- lib/sync.ts
+++ lib/sync.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import * as ws from 'ws';
+import * as WebSocket from 'ws';
 import * as EventEmitter from 'events';
 
 interface LogEntry {
@@ -14,19 +14,36 @@ export interface SessionLogEntry extends LogEntry {
     session: any;
   }
 }
-const emitter = new EventEmitter();
+
+function isSessionLogEntry(entry: LogEntry): entry is SessionLogEntry {
+  return entry.type === 'session';
+}
+
+let connected = false;
+export const emitter = new EventEmitter();
 emitter.on('error', (e) => console.log(`EMITTER: error ${JSON.stringify(e)}`));
 
 export function updateSession(sid: string, session: any): void {
+  if (connected) {
+    console.log(`update session do nothing`);
+    return;
+  }
   const data = { sid, session };
   const sessionLogEntry: SessionLogEntry = { type: 'session', data };
   console.log(`updateSession log entry ${JSON.stringify(sessionLogEntry)}`);
   emitter.emit('session', sessionLogEntry);
 }
 
+export function updateSessionForNewClient(ws: WebSocket, sid: string, session: any): void {
+  const data = { sid, session };
+  const sessionLogEntry: SessionLogEntry = { type: 'session', data };
+  console.log(`updateSessionForNewClient log entry ${JSON.stringify(sessionLogEntry)}`);
+  ws.send(JSON.stringify(sessionLogEntry));
+}
+
 export class SyncEndpoint {
   constructor(
-    private ws: ws,
+    private ws: WebSocket,
     private req: express.Request,
   ) {
     this.init();
@@ -34,6 +51,7 @@ export class SyncEndpoint {
 
   private init(): void {
     const listener = this.onSessionChange.bind(this);
+    emitter.emit('connected', this.ws);
     emitter.addListener('session', listener);
     this.ws.on('close', () => emitter.removeListener('session', listener));
   }
@@ -41,6 +59,27 @@ export class SyncEndpoint {
   private onSessionChange(entry: SessionLogEntry) {
     console.log(`SyncEndpoint:onSessionChange: send to client entry ${JSON.stringify(entry)}`);
     this.ws.send(JSON.stringify(entry, null, 2));
+  }
+}
+
+export const syncEmitter = new EventEmitter();
+
+export class SyncClient {
+  private ws: WebSocket;
+
+  constructor() {
+    this.ws = new WebSocket('wss://localhost:8544/sync', { rejectUnauthorized: false });
+    this.ws.on('open', () => {
+      console.log('opened');
+      connected = true;
+    });
+    this.ws.on('message', (data) => {
+      console.log(`message ${data}`);
+      const entry: LogEntry = JSON.parse(data.toString());
+      if (isSessionLogEntry(entry)) {
+        syncEmitter.emit('session', entry.data);
+      }
+    });
   }
 }
 
