8e45ec06ca8d3dbb1ee961ad27cd3d63243fa5c8
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-04-15 22:09:38 +0500

    Sync Node Servers: Add copyrights
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- lib/sync-types.ts
+++ lib/sync-types.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 import { StorageDict, KeyVal } from './clusterManager';
 
 export interface LogEntry {
@@ -103,3 +113,13 @@ export function isStorageActionDeleteAll(action: StorageAction): action is Stora
 export function isStorageActionDelete(action: StorageAction): action is StorageActionDelete {
   return action.type === 'delete';
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
--- lib/sync.ts
+++ lib/sync.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 import * as EventEmitter from 'events';
 import { KeyVal } from './clusterManager';
 import { SessionLogEntry, StorageActionSetAll, StorageLogEntry, StorageActionSet, StorageActionDeleteAll, StorageActionDelete } from './sync-types';
@@ -71,3 +81,14 @@ export function deleteStorageForPlugin(pluginId: string, key: string): void {
   syncEventEmitter.emit('storage', storageLogEntry);
 }
 
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
