7f3764699634c1cead58809c2df49ee222db742c
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-04-15 22:09:38 +0500

    Sync Node Servers: Add copyrights
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- lib/clusterManager.d.ts
+++ lib/clusterManager.d.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 import { EventEmitter } from "events";
 
 interface KeyVal {
@@ -16,3 +26,14 @@ declare class ClusterManager extends EventEmitter {
   getStorageCluster: () => Promise<StorageDict>;
 }
 
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
