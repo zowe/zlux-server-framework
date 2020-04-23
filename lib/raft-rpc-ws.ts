7781e869147bf56978feaab1f0801a3392a014dd
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-04-23 17:25:37 +0500

    PoC for Leader Election: RPC over Websocket
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

---
+++ lib/raft-rpc-ws.ts
@@ -0,0 +1,227 @@
+import * as WebSocket from 'ws';
+import * as express from 'express';
+import { RequestVoteArgs, RequestVoteReply, AppendEntriesArgs, AppendEntriesReply, RaftRPCDriver, Raft } from './raft';
+const zluxUtil = require('./util');
+const raftLog = zluxUtil.loggers.raftLogger;
+
+enum WebSocketMessageType {
+  RequestVoteArgs,
+  RequestVoteReply,
+  AppendEntriesArgs,
+  AppendEntriesReply,
+};
+
+interface WebSocketMessage {
+  seq: number;
+  type: 'RequestVoteArgs' | 'RequestVoteReply' | 'AppendEntriesArgs' | 'AppendEntriesReply';
+  // type: WebSocketMessageType,
+  message: any;
+}
+
+interface WebSocketRequestVoteArgsMessage extends WebSocketMessage {
+  type: 'RequestVoteArgs';
+  // type: WebSocketMessageType.RequestVoteArgs
+  message: RequestVoteArgs;
+}
+
+interface WebSocketRequestVoteReplyMessage extends WebSocketMessage {
+  type: 'RequestVoteReply';
+  // type: WebSocketMessageType.RequestVoteReply
+  message: RequestVoteReply;
+}
+
+function isWebSocketRequestVoteArgsMessage(message: WebSocketMessage): message is WebSocketRequestVoteArgsMessage {
+  return message.type === 'RequestVoteArgs';
+}
+
+interface WebSocketAppendEntriesArgsMessage extends WebSocketMessage {
+  type: 'AppendEntriesArgs';
+  // type: WebSocketMessageType.AppendEntriesArgs
+  message: AppendEntriesArgs;
+}
+
+interface WebSocketAppendEntriesReplyMessage extends WebSocketMessage {
+  type: 'AppendEntriesReply';
+  // type: WebSocketMessageType.AppendEntriesReply
+  message: AppendEntriesReply;
+}
+
+function isWebSocketAppendEntriesArgsMessage(message: WebSocketMessage): message is WebSocketAppendEntriesArgsMessage {
+  return message.type === 'AppendEntriesArgs';
+}
+
+interface PendingRequest {
+  message: WebSocketMessage;
+  resolve: (data: WebSocketMessage) => void;
+  reject: (err: Error) => void;
+}
+
+export class RaftRPCWebSocketDriver implements RaftRPCDriver {
+  private static seq = 1;
+  private ws: WebSocket;
+  private address: string;
+  private isConnected = false;
+
+  private pendingRequests = new Map<number, PendingRequest>();
+
+
+  constructor(
+    public host: string,
+    public port: number,
+    public secure: boolean
+  ) {
+    this.address = this.makeWebsocketAddress();
+  }
+
+  async sendRequestVote(args: RequestVoteArgs): Promise<RequestVoteReply> {
+    const message: WebSocketMessage = {
+      seq: RaftRPCWebSocketDriver.seq++,
+      type: 'RequestVoteArgs',
+      message: args,
+    };
+    return this.call(message).then(reply => reply.message);
+  }
+
+  async sendAppendEntries(args: AppendEntriesArgs): Promise<AppendEntriesReply> {
+    const message: WebSocketMessage = {
+      seq: RaftRPCWebSocketDriver.seq++,
+      type: 'AppendEntriesArgs',
+      message: args,
+    };
+    return this.call(message).then(reply => reply.message);
+  }
+
+  private makeWebsocketAddress(): string {
+    return `${this.secure ? 'wss' : 'ws'}://${this.host}:${this.port}/raft`;
+  }
+
+  private async connect(): Promise<void> {
+    if (this.isConnected) {
+      return Promise.resolve();
+    }
+    return new Promise<void>((resolve, reject) => {
+      this.ws = new WebSocket(this.address, { rejectUnauthorized: false });
+      this.ws.on('open', () => {
+        this.onOpen();
+        resolve();
+      });
+      this.ws.on('error', () => reject());
+      this.ws.on('message', (data: Buffer) => this.onMessage(data));
+      this.ws.on('close', (code: number, reason: string) => this.onClose(code, reason));
+    });
+  }
+
+  private async call(message: WebSocketMessage): Promise<WebSocketMessage> {
+    await this.connect();
+    const promise = new Promise<WebSocketMessage>((resolve, reject) => {
+      const seq = message.seq;
+      const pendingRequest: PendingRequest = { message, resolve, reject };
+      this.pendingRequests.set(seq, pendingRequest);
+    });
+    raftLog.info(`send websocket message ${JSON.stringify(message)} to ${this.address}`);
+    this.ws.send(JSON.stringify(message));
+    return promise;
+  }
+
+  private onOpen(): void {
+    this.isConnected = true;
+    raftLog.info(`connection to ${this.address} established`);
+  }
+
+  private onMessage(data: Buffer): void {
+    raftLog.info(`message ${data}`);
+    let message: WebSocketMessage;
+    try {
+      message = JSON.parse(data.toString());
+    } catch (e) {
+      raftLog.error(`ignore invalid message`);
+      return;
+    }
+    const seq = message.seq;
+    const pendingRequest = this.pendingRequests.get(seq);
+    if (!pendingRequest) {
+      raftLog.error(`no request found with seq ${seq}, ignore it`);
+      return;
+    }
+    this.pendingRequests.delete(seq);
+    pendingRequest.resolve(message);
+    raftLog.info(`resolve pending request with seq ${seq}, ignore it`);
+  }
+
+  private onClose(code: number, reason: string): void {
+    raftLog.info(`connection to ${this.address} closed ${code} ${reason}`);
+    this.isConnected = false;
+    this.ws = undefined;
+    this.pendingRequests.forEach((request => {
+      request.reject(new Error('connection closed'));
+    }));
+    this.pendingRequests.clear();
+  }
+
+  private onError(ws: WebSocket, err: Error): void {
+    raftLog.info(`connection error ${JSON.stringify(err)}`);
+  }
+
+}
+
+export class RaftRPCWebSocketService {
+  constructor(
+    private clientWS: WebSocket,
+    private req: express.Request,
+    private raft: Raft,
+  ) {
+    this.init();
+  }
+
+  private init(): void {
+    this.clientWS.on('close', () => this.onClose());
+    this.clientWS.on('message', (data: Buffer) => this.onMessage(data));
+  }
+
+  private onClose(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  private onMessage(data: Buffer): void {
+    raftLog.info(`message ${data}`);
+    let message: WebSocketMessage;
+    try {
+      message = JSON.parse(data.toString());
+    } catch (e) {
+      raftLog.error(`ignore invalid message`);
+      return;
+    }
+    raftLog.info(`got message ${JSON.stringify(message)}`);
+    if (isWebSocketRequestVoteArgsMessage(message)) {
+      raftLog.info(`got request vote message ${JSON.stringify(message)}`);
+      this.processRequestVoteMessage(message);
+    } else if (isWebSocketAppendEntriesArgsMessage(message)) {
+      raftLog.info(`got append entries message ${JSON.stringify(message)}`);
+      this.processAppendEntriesMessage(message);
+    }
+
+  }
+  private processAppendEntriesMessage(message: WebSocketAppendEntriesArgsMessage): void {
+    const seq = message.seq;
+    const args = message.message;
+    const reply = this.raft.appendEntries(args);
+    const replyMessage: WebSocketAppendEntriesReplyMessage = {
+      type: 'AppendEntriesReply',
+      seq,
+      message: reply,
+    };
+    this.clientWS.send(JSON.stringify(replyMessage));
+  }
+
+  private processRequestVoteMessage(message: WebSocketRequestVoteArgsMessage): void {
+    const seq = message.seq;
+    const args = message.message;
+    const reply = this.raft.requestVote(args);
+    const replyMessage: WebSocketRequestVoteReplyMessage = {
+      type: 'RequestVoteReply',
+      seq,
+      message: reply,
+    };
+    this.clientWS.send(JSON.stringify(replyMessage));
+  }
+}
--- lib/raft.ts
+++ lib/raft.ts
@@ -8,14 +8,54 @@
   Copyright Contributors to the Zowe Project.
 */
 
-import { EventEmitter } from "events";
+import { RaftRPCWebSocketDriver } from "./raft-rpc-ws";
+const zluxUtil = require('./util');
+const raftLog = zluxUtil.loggers.raftLogger;
 
-export interface RaftPeer {
-
+export class RaftPeer extends RaftRPCWebSocketDriver {
+  constructor(
+    host: string,
+    port: number,
+    secure: boolean
+  ) {
+    super(host, port, secure);
+  }
 }
 
 export interface RaftLogEntry {
+  Term: number;
+  Command: any;
+}
 
+export interface RequestVoteArgs {
+  term: number; // candidate’s term
+  candidateId: number; // candidate requesting vote
+  lastLogIndex?: number; // index of candidate’s last log entry (§5.4)
+  lastLogTerm?: number; //term of candidate’s last log entry (§5.4)
+}
+
+export interface RequestVoteReply {
+  Term: number;  // currentTerm, for candidate to update itself
+  VoteGranted: boolean; // true means candidate received vote
+}
+
+export interface AppendEntriesArgs {
+  Term: number;        // Leader’s term
+  LeaderID: number;        // so follower can redirect clients
+  PrevLogIndex: number;        // index of log entry immediately preceding new ones
+  PrevLogTerm: number;        // term of prevLogIndex entry
+  Entries: RaftLogEntry[]; // entries to store (empty for heartbeat; may send more than one for efficiency)
+  LeaderCommit: number;        // leader’s commitIndex
+}
+
+export interface AppendEntriesReply {
+  Term: number;  // currentTerm, for leader to update itself
+  Success: boolean; // true if follower contained entry matching prevLogIndex and prevLogTerm
+}
+
+export interface RaftRPCDriver {
+  sendRequestVote: (args: RequestVoteArgs) => Promise<RequestVoteReply>;
+  sendAppendEntries: (args: AppendEntriesArgs) => Promise<AppendEntriesReply>;
 }
 
 export type State = 'Leader' | 'Follower' | 'Candidate'
@@ -25,16 +65,15 @@ const maxElectionTimeout = 300;
 
 export class Raft {
   private peers: RaftPeer[]; // RPC end points of all peers
-  private me: number;                             // this peer's index into peers[]
+  private me: number;  // this peer's index into peers[]
   private state: State = 'Follower'
   private readonly electionTimeout = Math.floor(Math.random() * (maxElectionTimeout - minElectionTimeout) + minElectionTimeout);
-  private heartBeatEmitter = new EventEmitter();
   private debug = true
 
   // persistent state
   private currentTerm: number = 0;
   private votedFor = -1
-  private log: RaftLogEntry[];
+  private log: RaftLogEntry[] = [];
 
   // volatile state on all servers
   private commitIndex: number = -1;
@@ -47,20 +86,25 @@ export class Raft {
   private readonly heartbeatInterval: number = 50;
   private heartbeatTimeoutId: NodeJS.Timer;
 
-  static make(peers: RaftPeer[], me: number): Raft {
-    const raft = new Raft();
-    raft.startElectionOnTimeout();
-    return raft;
-  }
 
   constructor() {
 
   }
 
-  startElectionOnTimeout(): void {
+  start(peers: RaftPeer[], me: number): void {
+    raftLog.info(`starting peer ${me}`);
+    this.peers = peers;
+    this.me = me;
+    this.scheduleElectionOnTimeout();
+  }
+
+  private scheduleElectionOnTimeout(): void {
+    if (this.isLeader()) {
+      return;
+    }
     this.electionTimeoutId = setTimeout(() => {
       if (this.isLeader()) {
-        this.startElectionOnTimeout();
+        // this.scheduleElectionOnTimeout();
       } else {
         this.attemptElection();
       }
@@ -109,6 +153,7 @@ export class Raft {
         this.convertToLeader();
       });
     }
+    this.scheduleElectionOnTimeout();
   }
 
   convertToLeader(): void {
@@ -131,9 +176,7 @@ export class Raft {
           return;
         }
         await this.callAppendEntries(server, this.currentTerm, -1);
-        if (false) {
-          this.print("got heartbeat response from %d at term %d", server, this.currentTerm);
-        }
+        // this.print("got heartbeat response from %d at term %d", server, this.currentTerm);
       });
     }
     if (!this.isLeader()) {
@@ -143,12 +186,119 @@ export class Raft {
     this.heartbeatTimeoutId = setTimeout(() => this.sendHeartbeat(), this.heartbeatInterval)
   }
 
-  async callAppendEntries(server: number, currentTerm: any, arg2: number): Promise<void> {
-
+  async callAppendEntries(server: number, currentTerm: any, index: number): Promise<boolean> {
+    const entries: RaftLogEntry[] = [];
+    if (index != -1) {
+      entries.push(this.log[index])
+    }
+    const prevLogIndex = index - 1;
+    let prevLogTerm = -1;
+    if (prevLogIndex >= 0) {
+      prevLogTerm = this.log[prevLogIndex].Term
+    }
+    const args: AppendEntriesArgs = {
+      LeaderID: this.me,
+      Term: this.currentTerm,
+      Entries: entries,
+      LeaderCommit: this.commitIndex,
+      PrevLogIndex: prevLogIndex,
+      PrevLogTerm: prevLogTerm,
+    };
+    const peer = this.peers[server];
+    return peer.sendAppendEntries(args).then(reply => reply.Success).catch(() => false);
   }
 
   async callRequestVote(server: number, term: number): Promise<boolean> {
-    return Promise.resolve(true);
+    const peer = this.peers[server];
+    const requestVoteArgs: RequestVoteArgs = {
+      candidateId: this.me,
+      term: term,
+    }
+    return peer.sendRequestVote(requestVoteArgs)
+      .then(reply => reply.VoteGranted)
+      .catch(() => false);
+  }
+
+  appendEntries(args: AppendEntriesArgs): AppendEntriesReply {
+    this.print("got append entries request from leader %d at term %d", args.LeaderID, args.Term)
+    // 1. Reply false if term < currentTerm (§5.1)
+    if (args.Term < this.currentTerm) {
+      return {
+        Success: false,
+        Term: this.currentTerm,
+      }
+    }
+    if (args.PrevLogIndex >= 0) {
+      // 2. Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm (§5.3)
+      if (args.PrevLogIndex >= this.log.length) {
+        return {
+          Success: false,
+          Term: this.currentTerm,
+        }
+      }
+      // 3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it (§5.3)
+      const prevLogTerm = this.log[args.PrevLogIndex].Term;
+      if (prevLogTerm != args.PrevLogTerm) {
+        this.log = this.log.slice(0, args.PrevLogIndex);
+      }
+    }
+    // 4. Append any new entries not already in the log
+    this.log = this.log.concat(args.Entries);
+    // 5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
+    const lastNewEntryIndex = this.log.length - 1;
+    if (args.LeaderCommit > this.commitIndex) {
+      this.commitIndex = Math.min(args.LeaderCommit, lastNewEntryIndex);
+    }
+
+    this.convertToFollower();
+    return {
+      Success: true,
+      Term: args.Term,
+    };
+  }
+
+  convertToFollower(): void {
+    this.state = 'Follower';
+    this.cancelCurrentElectionTimeoutAndReschedule();
+    this.cancelHeartbeat();
+  }
+
+  cancelHeartbeat(): void {
+    if (this.heartbeatTimeoutId) {
+      clearTimeout(this.heartbeatTimeoutId);
+      this.heartbeatTimeoutId = undefined;
+    }
+  }
+
+  cancelCurrentElectionTimeoutAndReschedule(): void {
+    clearTimeout(this.electionTimeoutId);
+    this.scheduleElectionOnTimeout();
+  }
+
+  requestVote(args: RequestVoteArgs): RequestVoteReply {
+    this.print("got vote request from %d at term %d, my term is %d", args.candidateId, args.term, this.currentTerm)
+    if (args.term < this.currentTerm) {
+      this.print("got vote request from %d at term %d", args.candidateId, args.term);
+      return {
+        Term: this.currentTerm,
+        VoteGranted: false,
+      };
+    }
+    if (args.term > this.currentTerm) {
+      this.votedFor = -1;
+    }
+    if (this.votedFor == -1 || this.votedFor == this.me) {
+      this.votedFor = args.candidateId;
+      this.currentTerm = args.term;
+      return {
+        Term: this.currentTerm,
+        VoteGranted: true
+      };
+    }
+    return {
+      Term: this.currentTerm,
+      VoteGranted: false,
+    };
   }
 
   private print(...args: any[]): void {
@@ -158,6 +308,15 @@ export class Raft {
   }
 
 }
+
+
+export const peers: RaftPeer[] = [
+  new RaftPeer('localhost', 8544, true),
+  new RaftPeer('localhost', 8545, true),
+  new RaftPeer('localhost', 8546, true),
+];
+
+export const raft = new Raft();
 
 /*
   This program and the accompanying materials are
