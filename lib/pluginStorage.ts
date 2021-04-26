e37996842814254fdf24873213569a893e46d74c
1000TurquoisePogs <sgrady@rocketsoftware.com>
2021-04-26 18:22:43 -0400

    Add changelog update, licensing
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- lib/pluginStorage.ts
+++ lib/pluginStorage.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
 import * as apimlStorage from './apimlStorage';
 
 
@@ -330,7 +340,6 @@ export function PluginStorageFactory(pluginId: string, logger): IPluginStorage {
 }
 
 export function LocalStorageFactory(id?: string): ILocalStorage {
-  
   const storageDict = {};
     
   if (id) {
