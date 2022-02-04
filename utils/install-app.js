2b7b95e85ef40a9c4eceeae1ef08138476115fca
Leonty Chudinov <lchudinov@rocketsoftware.com>
2022-02-04 10:40:06 +0500

    Use instanceDir
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- utils/install-app.js
+++ utils/install-app.js
@@ -151,8 +151,8 @@ function copyRecognizers(appDir, appId, appVers) {
   let configRecognizers;
   let configLocation;
 
-  if (process.env.INSTANCE_DIR) {
-    configLocation = path.join(process.env.INSTANCE_DIR, "workspace/app-server/ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/");
+  if (process.env.ZWED_instanceDir) {
+    configLocation = path.join(process.env.ZWED_instanceDir, "ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/");
   } else {
     try {
       let instanceDir = JSON.parse(fs.readFileSync(userInput.zluxConfig)).instanceDir;
@@ -225,8 +225,8 @@ function copyActions(appDir, appId, appVers) {
     logger.debug("Could not find actions in '" + (path.join(appDir, "config/actions")) + "'");
   }
 
-  if (process.env.INSTANCE_DIR) {
-    configLocation = path.join(process.env.INSTANCE_DIR, "workspace/app-server/ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/");
+  if (process.env.ZWED_instanceDir) {
+    configLocation = path.join(process.env.ZWED_instanceDir, "ZLUX/pluginStorage/org.zowe.zlux.ng2desktop/");
   } else {
     try {
       let instanceDir = JSON.parse(fs.readFileSync(userInput.zluxConfig)).instanceDir;
