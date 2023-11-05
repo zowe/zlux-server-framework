b07a0ae3f6aafbdb78c62ea379521fcb5c563f3a
Himani1519 <paripathak80@gmail.com>
2023-11-05 17:23:18 +0530

    fix spacing issue
    Signed-off-by: Himani1519 <paripathak80@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
         '\n  This program and the accompanying materials are' +
        '\n  made available under the terms of the Eclipse Public License v2.0 which accompanies' +
        '\n  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html' +
-       '\n\n  SPDX-License-Identifier: EPL-2.0\n\n  Copyright Contributors to the Zowe Project.\n'
+       '\n  \n  SPDX-License-Identifier: EPL-2.0\n  \n  Copyright Contributors to the Zowe Project.\n'
      ],
       2,
     ],
